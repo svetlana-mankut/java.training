5bd39300521d1896cc41c38cd8b00b2e7ae22a68
polkota <svetlanamankut@gmail.com>
2016-05-29 00:23:06 +0300

    added new classes

---
+++ sandbox/src/main/java/ua/qa/Square.java
@@ -0,0 +1,16 @@
+package ua.qa;
+
+/**
+ * Created by polkota on 28.05.2016.
+ */
+public class Square {
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+
+    public double area() {
+        return this.l * this.l;
+    }
+}
