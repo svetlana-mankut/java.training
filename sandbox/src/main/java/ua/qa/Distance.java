7aad22502fca917521e55054e3bc04ddc12ca6c6
polkota <svetlanamankut@gmail.com>
2016-05-29 02:28:08 +0300

    added distance 2nd version

--- sandbox/src/main/java/ua/qa/Distance.java
+++ sandbox/src/main/java/ua/qa/Distance.java
@@ -8,10 +8,8 @@ public class Distance {
 
         Point p1 = new Point(3, 9);
         Point p2 = new Point(1, 8);
-        System.out.println("Расстояние между точками" + " = " + distance(p1, p2));
+        System.out.println("Расстояние между точками" + " = " + p1.distance(p2));
     }
 
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt(Math.pow((p1.x - p2.x), (2)) + Math.pow((p1.y - p2.y), (2)));
-    }
+
 }
--- sandbox/src/main/java/ua/qa/Point.java
+++ sandbox/src/main/java/ua/qa/Point.java
@@ -8,10 +8,15 @@ public class Point {
     public double y;
 
 
+
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
 
+
+    }
+    public double distance(Point p2) {
+        return Math.sqrt(Math.pow((this.x - p2.x), (2)) + Math.pow((this.y - p2.y), (2)));
     }
 }
 
