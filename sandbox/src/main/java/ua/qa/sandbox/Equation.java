5511024173dcee75ef6aecba031c8505d8e86ba2
polkota <svetlanamankut@gmail.com>
2016-07-26 16:29:54 +0300

    l3m2: if-else update

--- sandbox/src/main/java/ua/qa/sandbox/Equation.java
+++ sandbox/src/main/java/ua/qa/sandbox/Equation.java
@@ -4,32 +4,47 @@ package ua.qa.sandbox;
  * Created by polkota on 26.07.2016.
  */
 public class Equation {
-    private  double a;
-    private  double b;
-    private  double c;
+    private double a;
+    private double b;
+    private double c;
 
     private int n;
 
-    public Equation(double a, double b, double c){
+    public Equation(double a, double b, double c) {
 
         this.a = a;
         this.b = b;
         this.c = c;
 
-        double d = b*b - 4*a*c;
+        double d = b * b - 4 * a * c;
 
-        if (d > 0){
-            n = 2;
+        if (a == 0){
+            if (b == 0){
+                if (c == 0){
+                        n = -1;
+                } else {
+                    n = 0;
+                }
+            } else {
+                n = 1;
+            }
+
         } else {
-            if (d == 0){
+            if (d > 0) {
+                n = 2;
+            } else if (d == 0) {
                 n = 1;
             } else {
                 n = 0;
             }
         }
+
+
+
     }
 
-    public int rootNumber(){
+
+    public int rootNumber() {
         return n;
     }
 }
