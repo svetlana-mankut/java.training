b2ae504be76c69d36eb3757876e07e12e13ec18b
polkota <svetlanamankut@gmail.com>
2016-07-26 16:11:28 +0300

    l2m1: if-else конструкция добавлена в sandbox

---
+++ sandbox/src/test/java/ua/qa/sandbox/EquationTests.java
@@ -0,0 +1,26 @@
+package ua.qa.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by polkota on 26.07.2016.
+ */
+public class EquationTests {
+    @Test
+    public void test0(){
+        Equation e = new Equation(1,1,1);
+        Assert.assertEquals(e.rootNumber(), 0);
+    }
+
+    @Test
+    public void test1(){
+        Equation e = new Equation(1,2,1);
+        Assert.assertEquals(e.rootNumber(), 1);
+    }
+    @Test
+    public void test2(){
+        Equation e = new Equation(1,5,6);
+        Assert.assertEquals(e.rootNumber(), 2);
+    }
+}
