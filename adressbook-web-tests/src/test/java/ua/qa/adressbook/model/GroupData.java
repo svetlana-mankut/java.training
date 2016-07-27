a139427c58f112ede2a0791719819335b402a441
polkota <svetlanamankut@gmail.com>
2016-07-27 18:23:24 +0300

    l4m10: добавлена сортировка списков при создании и модификации групп

--- adressbook-web-tests/src/test/java/ua/qa/adressbook/model/GroupData.java
+++ adressbook-web-tests/src/test/java/ua/qa/adressbook/model/GroupData.java
@@ -16,8 +16,9 @@ public class GroupData {
         this.header = header;
         this.footer = footer;
     }
-    public GroupData( String name, String header, String footer) {
-        this.id = 0;
+
+     public GroupData(String name, String header, String footer) {
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -37,25 +38,6 @@ public class GroupData {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        if (id != groupData.id) return false;
-        return name != null ? name.equals(groupData.name) : groupData.name == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
-    }
-
-    @Override
     public String toString() {
         return "GroupData{" +
                 "id='" + id + '\'' +
@@ -63,7 +45,21 @@ public class GroupData {
                 '}';
 
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        GroupData groupData = (GroupData) o;
+
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 
 
 }
