087c645cfe67dc0a10f9574550e6443e8e2cd81b
polkota <svetlanamankut@gmail.com>
2016-08-03 11:42:19 +0300

    l6m8

--- adressbook-web-tests/src/test/java/ua/qa/adressbook/generators/ContactDataGenerator.java
+++ adressbook-web-tests/src/test/java/ua/qa/adressbook/generators/ContactDataGenerator.java
@@ -59,18 +59,18 @@ public class ContactDataGenerator {
         XStream xstream = new XStream();
         xstream.processAnnotations(ContactData.class);
         String xml = xstream.toXML(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
     }
 
     private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
-        Writer writer = new FileWriter(file);
-        for (ContactData contact : contacts) {
-            writer.write(String.format("%s;%s;%s\n", contact.getFirstname(), contact.getLastname()));
-        }
-        writer.close();
+       try(Writer writer = new FileWriter(file)) {
+           for (ContactData contact : contacts) {
+               writer.write(String.format("%s;%s;%s\n", contact.getFirstname(), contact.getLastname()));
+           }
+       }
     }
 
     private List<ContactData> generateContacts(int count) {
